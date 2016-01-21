82838cdbce98b533540191d4f1bf10096487e536
蟯ｸ諷ｶ邏 <innovation.kishi@gmail.com>
2016-01-21 20:16:47 +0900

    Initial Commit

---
+++ Maskat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maskat
+//
+//  Created by YoshiakiKishi on 2016/01/21.
+//  Copyright © 2016年 Yoshua Dilham Kishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
